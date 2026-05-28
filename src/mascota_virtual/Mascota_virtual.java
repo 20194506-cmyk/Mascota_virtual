bc0603778173f6766e9d85aa2a33e569f363b9cf
Jose <jf3034020@gmail.com>
2026-05-28 17:42:20 -0600

    Solucionando conflicto y preparando juego.java

--- src/mascota_virtual/Mascota_virtual.java
+++ src/mascota_virtual/Mascota_virtual.java
@@ -1,10 +1,12 @@
 package mascota_virtual;
-
+import java.util.Scanner;
 public class Mascota_virtual {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+Scanner sc=new Scanner(System.in);
 
+System.out.println("Te gane todo en clash royale");
 	}
 
 }
